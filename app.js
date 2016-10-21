f4cc85c581af36f04f6891c92bdbf51fe384a09c
Ru Tan <jutan2012@gmail.com>
2016-10-21 15:22:03 +0200

    Ajout app.js  Ameliorer la version

---
+++ app.js
@@ -0,0 +1,43 @@
+(function(){	
+	var app = {
+		jour:null,
+		week:["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+		mounth:null,
+		year:null,
+		init:function(){
+			app.listeners();
+		},
+		listeners:function(){
+			$("#button").on('click',function(){
+				app.jour = $("#jour").val();
+				app.mounth = $("#mois").val();
+				app.year = $("#annee").val();
+				app.verif();
+				app.affiche();
+			});
+		},
+		verif: function(){
+			if(app.jour >= 1 && app.jour <= 31){
+
+			}else {
+				console.log("Error");
+			}
+			if(app.year > 0){	
+			
+			}else {
+				console.log("Error");
+			};
+			console.log(app.jour+'/'+app.mounth+'/'+app.year);
+		},
+		affiche: function(){
+			var date = moment(app.jour+'/'+app.mounth+'/'+app.year);
+			var dow = date.day();
+			console.log(dow);
+
+		},
+
+
+
+	};
+	app.init();
+})();
